8e74543646ab8ae783b3e8cf7df95d026e068d88
pietrociatta <59965830+pietrociatta@users.noreply.github.com>
2022-05-31 22:25:42 +0200

    finished create-nft page fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,8 +13,6 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off',
     'no-console': 0,
     'jsx-a11y/label-has-associated-control': 0,
     'no-nested-ternary': 0,
